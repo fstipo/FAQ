fb4c12614d919a521fdae8632f60bdd11dcb20e2
fstipo <fstipanov@gmail.com>
2022-03-01 12:12:23 +0100

    add js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-const btn = document.querySelector('span');
+const btns = document.querySelectorAll('span');
+const answer = document.querySelector('.answer');
+const p = document.querySelector('p');
+
+const showAnswer = () => {
+  answer.style.display = 'flex';
+  p.style.marginBottom = '2rem';
+};
+
+btns.forEach((el) => {
+  el.addEventListener('click', () => {
+    answer.style.display = 'flex';
+    p.style.marginBottom = '2rem';
+  });
+  console.log(el);
+  //   btn.addEventListener('click', showAnswer);
+});
